8813f091682033c42919bfeca29a45ff98c35067
Farhan Khan <farhan.k2005@gmail.com>
2023-01-14 21:10:42 -0500

    learned about multiple components

--- Part-1/src/App.js
+++ Part-1/src/App.js
@@ -1,8 +1,23 @@
-const App = () =>(
-
+const Greet = (props) => {
+  return (
     <div>
-        <h1> Hello World </h1>
-    </div>    
-)
+      <h1>Greetings {props.name}!</h1>
+      <p>It is very nice to meet you. I see your are {props.age} years old!</p>
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  const name = "Farhan";
+  const age = 69;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Greet name="Maya" age={26 + 10} />
+      <Greet name={name} age={age} />
+    </div>
+  );
+};
+
+export default App;
