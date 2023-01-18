9bae1d8dfd45f2f803b411794281d059ac3e27e0
Farhan Khan <farhan.k2005@gmail.com>
2023-01-17 22:55:17 -0500

    learned about stateful components & event handling

--- Part-1/src/App.js
+++ Part-1/src/App.js
@@ -1,27 +1,20 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={58} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>+</button>
+      <button onClick={setToZero}>reset</button>
     </div>
   );
-};
+}
 
 export default App;
