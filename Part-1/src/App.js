9c6376d817683143fef82b9ae8c5747d78e360d3
Farhan Khan <farhan.k2005@gmail.com>
2023-01-17 23:12:15 -0500

    learned how changes in state causes the entire component to re-render

--- Part-1/src/App.js
+++ Part-1/src/App.js
@@ -1,20 +1,39 @@
 import { useState } from "react";
 
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.name}</button>;
+};
+
 const App = () => {
-  
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
 
-  const setToZero = () => setCounter(0);
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
 
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>+</button>
-      <button onClick={setToZero}>reset</button>
+      <Display counter={counter} />
+      <Button name="+" onClick={increaseByOne} />
+      <Button name="reset" onClick={setToZero} />
+      <Button name="-" onClick={decreaseByOne} />
     </div>
   );
-}
+};
 
 export default App;
