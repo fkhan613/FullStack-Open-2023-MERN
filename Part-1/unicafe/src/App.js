77eaa14a826e29a3b9cf1638bfa4ab0302ecf534
Farhan Khan <farhan.k2005@gmail.com>
2023-01-22 23:44:31 -0500

    finished 1.7

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,41 +1,50 @@
 import { useState } from "react";
 
-const Button = ({onClick, name}) =>{
-  return(
-    <button onClick={onClick}>{name}</button>
-  );
-}
+const Button = ({ onClick, name }) => {
+  return <button onClick={onClick}>{name}</button>;
+};
 
-const Statistics = ({good, bad, neutral}) =>{
+const Statistics = ({ good, bad, neutral, all, positive, average }) => {
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Bad: {bad}</p>
       <p>Neutral: {neutral}</p>
+      <p>All: {all}</p>
+      <p>Average: {isNaN(average) ? 0 : average}</p>
+      <p>Positive: {isNaN(positive) ? 0 : positive}%</p>
     </div>
   );
-}
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    bad: 0,
+    neutral: 0,
+    all: 0,
+  });
 
   const handleGoodClick = () => {
-    let copy = good;
-    setGood(copy + 1);
+    let copy = { ...feedback, good: feedback.good + 1, all: feedback.all + 1 };
+    setFeedback(copy);
   };
 
   const handleBadClick = () => {
-    let copy = bad;
-    setBad(copy + 1);
+    let copy = { ...feedback, bad: feedback.bad + 1, all: feedback.all + 1 };
+    setFeedback(copy);
   };
 
   const handleNeutralClick = () => {
-    let copy = neutral;
-    setNeutral(copy + 1);
+    let copy = {
+      ...feedback,
+      neutral: feedback.neutral + 1,
+      all: feedback.all + 1,
+    };
+    setFeedback(copy);
   };
 
   return (
@@ -44,7 +53,14 @@ const App = () => {
       <Button onClick={handleGoodClick} name="Good" />
       <Button onClick={handleBadClick} name="Bad" />
       <Button onClick={handleNeutralClick} name="Neutral" />
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <Statistics
+        good={feedback.good}
+        bad={feedback.bad}
+        neutral={feedback.neutral}
+        all={feedback.all}
+        average={(feedback.good+feedback.bad+feedback.neutral)/3}
+        positive={parseInt((feedback.good/feedback.all)*100)}
+      />
     </div>
   );
 };
