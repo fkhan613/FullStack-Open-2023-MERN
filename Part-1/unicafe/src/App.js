506c079543eff2c15cc70eaaafc7b0178d0bbe9e
Farhan Khan <farhan.k2005@gmail.com>
2023-01-22 22:58:13 -0500

    finished ex 1.6

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Button = ({onClick, name}) =>{
+  return(
+    <button onClick={onClick}>{name}</button>
+  );
+}
+
+const Statistics = ({good, bad, neutral}) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Bad: {bad}</p>
+      <p>Neutral: {neutral}</p>
     </div>
   );
 }
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    let copy = good;
+    setGood(copy + 1);
+  };
+
+  const handleBadClick = () => {
+    let copy = bad;
+    setBad(copy + 1);
+  };
+
+  const handleNeutralClick = () => {
+    let copy = neutral;
+    setNeutral(copy + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGoodClick} name="Good" />
+      <Button onClick={handleBadClick} name="Bad" />
+      <Button onClick={handleNeutralClick} name="Neutral" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
 
 export default App;
