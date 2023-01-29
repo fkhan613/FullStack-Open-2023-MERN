85960eb8cd5d927b6f37f8e78f3fcf7c38045d35
Farhan Khan <farhan.k2005@gmail.com>
2023-01-29 15:42:47 -0500

    finished ex 2.6

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,21 +1,47 @@
 import { useState } from "react";
+import Person from "./compenents/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if(newName === ""){
+       window.alert("Missing Fields") 
+       return false
+    }
+
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName("")
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          Name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+
+      <div>
+        {persons.map((person) => <Person key={person.name} name={person.name} />)}
+      </div>
+
     </div>
   );
 };
