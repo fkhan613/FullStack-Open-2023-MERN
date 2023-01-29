bf845eb6d061c9b6d147b3aef22a857c173a994b
Farhan Khan <farhan.k2005@gmail.com>
2023-01-29 14:47:04 -0500

    learned about filtering displayed elements

--- Part-2/lesson/src/App.js
+++ Part-2/lesson/src/App.js
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -14,19 +15,24 @@ const App = (props) => {
       id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote("")
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>{" "}
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
