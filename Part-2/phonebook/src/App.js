8d7f021640ee9acfa72a06f46936a5efc5e6dfa8
Farhan Khan <farhan.k2005@gmail.com>
2023-01-29 16:19:51 -0500

    ex 2.9 done, implemented live search

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Person from "./compenents/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "(613) 745-7489" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -42,12 +46,27 @@ const App = () => {
     return false;
   };
 
+  const peopleToShow =
+    searchQuery === ""
+      ? persons
+      : persons.filter(
+          (person) => person.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search:{""}
+        <input
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+        ></input>
+      </div>
+      <h2>Add a New Contact</h2>
       <form onSubmit={addPerson}>
         <div>
-          Name:{""}
+          Name: {""}
           <input
             value={newName}
             onChange={(event) => setNewName(event.target.value)}
@@ -67,7 +86,7 @@ const App = () => {
       <h2>Numbers</h2>
 
       <div>
-        {persons.map((person) => (
+        {peopleToShow.map((person) => (
           <Person key={person.name} name={person.name} number={person.number} />
         ))}
       </div>
