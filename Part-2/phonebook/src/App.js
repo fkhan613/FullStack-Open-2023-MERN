3917c99f7d171d97c624cd27e2709a690f455297
Farhan Khan <farhan.k2005@gmail.com>
2023-02-05 20:17:16 -0500

    we can GET and POST users from db

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./compenents/Filter";
 import AddPersonForm from "./compenents/AddPersonForm";
 import Contacts from "./compenents/Contacts";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const API_URL = "http://localhost:3001/persons";
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const getUsers = () => {
+    axios.get(API_URL).then((res) => {
+      setPersons(res.data);
+    });
+  };
+
+  const insertUser = (user) => {
+    axios
+      .post(API_URL, user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(getUsers, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -34,7 +49,8 @@ const App = () => {
       number: newNumber,
     };
 
-    setPersons(persons.concat(newPerson));
+    setPersons(persons.concat(newPerson))
+    insertUser(newPerson);
     resetFields();
   };
 
