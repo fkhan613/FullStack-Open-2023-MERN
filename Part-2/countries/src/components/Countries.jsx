6de48847a6a2458e51553208a2cb159f47f07d40
Farhan Khan <farhan.k2005@gmail.com>
2023-02-21 23:33:18 -0500

    put countires and country in components

--- Part-2/countries/src/components/Countries.jsx
+++ Part-2/countries/src/components/Countries.jsx
@@ -1,6 +1,8 @@
 import Country from "./Country";
 
 const Countries = ({ countries }) => {
+
+
   if (countries == null) {
     return null;
   }
@@ -11,12 +13,12 @@ const Countries = ({ countries }) => {
 
   if (countries.length > 1 && countries.length <= 20) {
     return (
-      <div className='countries'>
+      <div className="countries">
         {countries.map((country) => (
           <Country
+            country={country}
             showBox={true}
             key={country.name}
-            name={country.name}
           ></Country>
         ))}
       </div>
@@ -26,14 +28,7 @@ const Countries = ({ countries }) => {
   if (countries.length == 1) {
     return (
       <div>
-        <h2>{countries[0].name}</h2>
-        <p>Capital: {countries[0].capital}</p>
-        <p>Region: {countries[0].region}</p>
-        <h3>Languages:</h3>
-        {countries[0].languages.map((lang) => (
-          <li key={lang.name}>{lang.name}</li>
-        ))}
-        <img src={countries[0].flag} alt="Country Flag"></img>
+        <Country country={countries[0]} key={countries[0].name} showBox={false}></Country>
       </div>
     );
   }
--- Part-2/countries/src/components/Country.jsx
+++ Part-2/countries/src/components/Country.jsx
@@ -1,14 +1,23 @@
-const Country = (props) => {
-  if (props.showBox) {
+const Country = ({ country, showBox }) => {
+  if (showBox) {
     return (
       <>
-        <span>
-          <li>{props.name}</li>
-        </span>
-        <span>
+          <li>{country.name}</li>
           <button>Show More</button>
-        </span>
       </>
+    );
+  } else {
+    return (
+      <div>
+        <h2>{country.name}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Region: {country.region}</p>
+        <h3>Languages:</h3>
+        {country.languages.map((lang) => (
+          <li key={lang.name}>{lang.name}</li>
+        ))}
+        <img src={country.flag} alt="Country Flag"></img>
+      </div>
     );
   }
 };
