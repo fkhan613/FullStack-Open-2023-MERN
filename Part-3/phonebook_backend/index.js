23c192c15c730bf5f7718bc572fb8fdc41474096
Farhan Khan <farhan.k2005@gmail.com>
2023-02-27 21:00:01 -0500

    ex 3.1 done

---
+++ Part-3/phonebook_backend/index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let people = [
+  [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+    },
+  ],
+];
+
+const generateId = () => {
+  const maxId = people.length > 0 ? Math.max(...people.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>You want people? Go through the api route</h1>");
+});
+
+app.get("/api/people", (request, response) => {
+  response.json(people);
+});
+
+app.get("/api/people/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = people.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send("person not found");
+  }
+});
+
+app.delete("/api/people/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  people = people.filter((person) => person.id !== id);
+
+  response.status(204).end("person deleted successfully!");
+});
+
+app.post("/api/people", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  people = people.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
