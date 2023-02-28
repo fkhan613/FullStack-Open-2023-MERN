7e7549b9bf33660ae3659323ec1fd31db35f38f7
Farhan Khan <farhan.k2005@gmail.com>
2023-02-27 20:44:20 -0500

    u can add notes now

--- Part-3/notes_backend/index.js
+++ Part-3/notes_backend/index.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-
 let notes = [
   {
     id: 1,
@@ -21,6 +20,11 @@ let notes = [
     important: true,
   },
 ];
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -42,19 +46,27 @@ app.get("/api/notes/:id", (request, response) => {
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  
   const id = Number(request.params.id);
 
   notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end('Note deleted successfully!');
+  response.status(204).end("Note deleted successfully!");
 });
 
 app.post("/api/notes", (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  const body = request.body;
 
-  const note = request.body;
-  note.id = maxId + 1;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 
