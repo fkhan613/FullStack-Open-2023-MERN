57e4801d8509d03d8fb68f8390ce2e452e1ae1d8
Farhan Khan <farhan.k2005@gmail.com>
2023-03-09 21:00:56 -0500

    deleted unused variables and methods

--- Part-3/notes_backend/index.js
+++ Part-3/notes_backend/index.js
@@ -9,33 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-
-    id: 4,
-    content: "Test Note",
-    important: true,
-  },
-];
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
