d5830f5774ac22baf74d91720046f6b872d81e0b
Farhan Khan <farhan.k2005@gmail.com>
2023-06-03 23:01:50 -0400

    ex 4.1 done

--- Part-4/blog_backend/app.js
+++ Part-4/blog_backend/app.js
@@ -3,15 +3,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+mongoose.set("strictQuery", false);
 
 console.log("Connecting to MongoDB...");
 mongoose.connect(process.env.MONGODB_URI).then(() => {
