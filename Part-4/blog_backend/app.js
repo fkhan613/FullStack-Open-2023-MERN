7ea1980699fa5cf304262b4175f13e0787741ff6
Farhan Khan <farhan.k2005@gmail.com>
2023-06-04 21:23:19 -0400

    got controllers and middleware working

--- Part-4/blog_backend/app.js
+++ Part-4/blog_backend/app.js
@@ -3,31 +3,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blog");
 
 mongoose.set("strictQuery", false);
 
 console.log("Connecting to MongoDB...");
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((error) => {console.log("Error connecting to MongoDB:", error.message);});
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
