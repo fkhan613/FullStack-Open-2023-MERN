4525ca6cb9ab64b815ff64c2948f4b4d1990149a
Farhan Khan <farhan.k2005@gmail.com>
2023-06-04 21:34:06 -0400

    refactored all code and added structure

--- Part-4/blog_backend/app.js
+++ Part-4/blog_backend/app.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+const config = require("./utils/config");
+const { info, error } = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,20 +9,22 @@ const blogsRouter = require("./controllers/blog");
 
 mongoose.set("strictQuery", false);
 
-console.log("Connecting to MongoDB...");
+info("connecting to", config.MONGODB_URI);
+
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(config.MONGODB_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    info("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("Error connecting to MongoDB:", error.message);
+    error("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+module.exports = app;
